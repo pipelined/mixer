57f70bc2cd3cf84beaa43302eb3817e193b46991
Sergei Dudka <serg.dudk@gmail.com>
2019-10-18 08:11:57 +0200

    new-api: update with new implementation

--- mixer.go
+++ mixer.go
@@ -10,52 +10,76 @@ import (
 
 // Mixer summs up multiple channels of messages into a single channel.
 type Mixer struct {
-	out      chan *frame       // channel to send frames ready for mix
-	in       chan *inMessage   // channel to send incoming messages
-	frames   map[string]*frame // frames sinking data
-	done     []string          // done inputs ids
-	register chan string       // register new input input, buffered
-	outputID atomic.Value      // id of the pipe which is output of mixer
-	*frame                     // last processed frame
-	cancel   chan struct{}     // cancel is closed only when pump is interrupted
+	out chan *frame // channel to send frames ready for mix
+	// in  chan *message // channel to send incoming messages
 
-	m           sync.Mutex
-	sampleRate  int
+	done []string // done inputs ids
+	// register chan string   // register new input input, buffered
+	outputID atomic.Value // id of the pipe which is output of mixer
+	// cancel   chan struct{} // cancel is closed only when pump is interrupted
+
+	active int32 // number of active inputs
+
+	// m           sync.Mutex
+	inputs      map[string]*input // inputs
+	frame       *frame            // last processed frame
+	sampleRate  signal.SampleRate
 	numChannels int
 }
 
-type inMessage struct {
+type message struct {
 	inputID string
 	buffer  signal.Float64
 }
 
+type input struct {
+	id          string
+	numChannels int
+	frame       *frame
+}
+
 // frame represents a slice of samples to mix.
 type frame struct {
-	buffers  [][][]float64
+	sync.Mutex
+	buffer   signal.Float64
+	summed   int
 	expected int
 	next     *frame
 }
 
 // sum returns mixed samplein.
-func (f *frame) sum(numChannels int, bufferSize int) [][]float64 {
-	var sum float64
-	var frames float64
-	result := make([][]float64, numChannels)
-	for nc := 0; nc < int(numChannels); nc++ {
-		result[nc] = make([]float64, 0, bufferSize)
-		for bs := 0; bs < bufferSize; bs++ {
-			sum = 0
-			frames = 0
-			// additional check to sum shorten blockin.
-			for i := 0; i < len(f.buffers) && len(f.buffers[i][nc]) > bs; i++ {
-				sum = sum + f.buffers[i][nc][bs]
-				frames++
-			}
-			result[nc] = append(result[nc], sum/frames)
+func (f *frame) copySum(b signal.Float64) {
+	// shrink result buffer if needed.
+	if f.buffer.Size() > b.Size() {
+		for i := range f.buffer {
+			b[i] = b[i][:f.buffer.Size()]
 		}
 	}
-	f.buffers = nil
-	return result
+
+	// copy summed data.
+	for i := 0; i < b.NumChannels(); i++ {
+		for j := 0; j < b.Size(); j++ {
+			b[i][j] = f.buffer[i][j] / float64(f.summed)
+		}
+	}
+}
+
+func (f *frame) add(b signal.Float64) bool {
+	// expand frame buffer if needed.
+	if diff := b.Size() - f.buffer.Size(); diff > 0 {
+		for i := range f.buffer {
+			f.buffer[i] = append(f.buffer[i], make([]float64, diff)...)
+		}
+	}
+
+	// copy summed data.
+	for i := 0; i < b.NumChannels(); i++ {
+		for j := 0; j < b.Size(); j++ {
+			f.buffer[i][j] += b[i][j]
+		}
+	}
+	f.summed++
+	return f.expected == f.summed
 }
 
 const (
@@ -63,48 +87,73 @@ const (
 )
 
 // New returns new mixer.
-func New() *Mixer {
+func New(numChannels int) *Mixer {
 	m := Mixer{
-		frames:   make(map[string]*frame),
-		in:       make(chan *inMessage, 1),
-		register: make(chan string, maxInputs),
-		cancel:   make(chan struct{}),
+		frame: &frame{
+			buffer: make([][]float64, numChannels),
+		},
+		inputs:      make(map[string]*input),
+		numChannels: numChannels,
+		// in:     make(chan *message, 1),
+		// register: make(chan string, maxInputs),
+		// cancel: make(chan struct{}),
 	}
 	return &m
 }
 
-// Sink registers new input. SampleRate and NumChannels are propagated here, due to that Sink pipes should be called before Pump.
-func (m *Mixer) Sink(inputID string, sampleRate, numChannel int) (func([][]float64) error, error) {
-	m.m.Lock()
+// Sink registers new input. All inputs should have same number of channels.
+// If different number of channels is provided, error will be returned.
+func (m *Mixer) Sink(inputID string, sampleRate signal.SampleRate, numChannels int) (func(signal.Float64) error, error) {
+	// m.m.Lock() // WRITE
+	// defer m.m.Unlock()
 	m.sampleRate = sampleRate
-	m.numChannels = numChannel
-	m.m.Unlock()
-	m.register <- inputID
-	return func(b [][]float64) error {
-		select {
-		case m.in <- &inMessage{inputID: inputID, buffer: b}:
-			return nil
-		case <-m.cancel:
-			return io.ErrClosedPipe
-		}
-	}, nil
-}
-
-// Flush mixer data for defined source.
-func (m *Mixer) Flush(sourceID string) error {
-	if m.isOutput(sourceID) {
-		m.cancel = make(chan struct{})
-		return nil
+	m.frame.expected++
+	in := input{
+		id:          inputID,
+		frame:       m.frame,
+		numChannels: numChannels,
 	}
-	m.in <- &inMessage{inputID: sourceID}
-	return nil
+	// add new input.
+	m.inputs[inputID] = &in
+
+	return func(b signal.Float64) error {
+		in.frame.Lock()
+		done := in.frame.add(b)
+		// move input to the next frame.
+		if in.frame.next == nil {
+			in.frame.next = newFrame(atomic.LoadInt32(&m.active), m.numChannels)
+		}
+		in.frame.Unlock()
+
+		// send if done
+		if done {
+			m.out <- in.frame
+		}
+
+		in.frame = in.frame.next
+		return nil
+	}, nil
 }
 
 // Reset resets the mixer for another run.
 func (m *Mixer) Reset(sourceID string) error {
 	if m.isOutput(sourceID) {
 		m.out = make(chan *frame, 1)
-		go m.mix()
+	} else {
+		atomic.AddInt32(&m.active, 1)
+	}
+	return nil
+}
+
+// Flush mixer data for defined source.
+func (m *Mixer) Flush(sourceID string) error {
+	if !m.isOutput(sourceID) {
+		active := atomic.AddInt32(&m.active, -1)
+		in := m.inputs[sourceID]
+		resetExpectations(m.out, in.frame, active)
+		if active == 0 {
+			close(m.out)
+		}
 	}
 	return nil
 }
@@ -114,121 +163,43 @@ func (m *Mixer) isOutput(sourceID string) bool {
 }
 
 // Pump returns a pump function which allows to read the out channel.
-func (m *Mixer) Pump(outputID string) (func(bufferSize int) ([][]float64, error), int, int, error) {
-	m.m.Lock()
-	sampleRate := m.sampleRate
+func (m *Mixer) Pump(outputID string) (func(signal.Float64) error, signal.SampleRate, int, error) {
 	numChannels := m.numChannels
-	m.m.Unlock()
 	m.outputID.Store(outputID)
-	return func(bufferSize int) ([][]float64, error) {
+	return func(b signal.Float64) error {
 		// receive new buffer
 		f, ok := <-m.out
 		if !ok {
-			return nil, io.EOF
+			return io.EOF
 		}
-		return f.sum(numChannels, bufferSize), nil
-	}, sampleRate, numChannels, nil
-}
-
-// Interrupt impliments pipe.Interrupter.
-func (m *Mixer) Interrupt(sourceID string) error {
-	if !m.isOutput(sourceID) {
-		m.in <- &inMessage{inputID: sourceID}
-	} else {
-		close(m.cancel)
-	}
-	return nil
-}
-
-func (m *Mixer) mix() {
-	m.frame = &frame{}
-
-	// regsiter available inputs.
-register:
-	for {
-		select {
-		// add all scheduled inputs.
-		case s := <-m.register:
-			m.frames[s] = m.frame
-		default:
-			// add done inputs.
-			for _, inputID := range m.done {
-				m.frames[inputID] = m.frame
-			}
-			m.done = make([]string, 0, len(m.frames))
-
-			// now we have all frames, can initiate first frame.
-			m.frame.expected = len(m.frames)
-			break register
-		}
-	}
-
-	defer close(m.out)
-	// start main loop.
-	for {
-		select {
-		case <-m.cancel:
-			return
-		// register new input during runtime.
-		case s := <-m.register:
-			// add new input.
-			m.frames[s] = m.frame
-			resetExpectations(m.frame, len(m.frames))
-		case msg := <-m.in:
-			f := m.frames[msg.inputID]
-			if msg.buffer != nil {
-				f.buffers = append(f.buffers, msg.buffer)
-				m.frame = send(f, m.out, m.cancel)
-
-				// proceed input to next frame.
-				if f.next == nil {
-					f.next = newFrame(len(m.frames))
-				}
-				m.frames[msg.inputID] = f.next
-			} else {
-				// move input to done.
-				m.done = append(m.done, msg.inputID)
-				delete(m.frames, msg.inputID)
-				// reset expectations.
-				resetExpectations(f, len(m.frames))
-				// send frames.
-				m.frame = send(f, m.out, m.cancel)
-				if len(m.frames) == 0 {
-					// close(m.out)
-					return
-				}
-			}
-		}
-	}
+		m.frame = f
+		f.copySum(b)
+		return nil
+	}, m.sampleRate, numChannels, nil
 }
 
 // isReady checks if frame is completed.
 func (f *frame) isComplete() bool {
-	return f.expected > 0 && f.expected == len(f.buffers)
+	return f.expected > 0 && f.expected == f.summed
 }
 
 // newFrame generates new frame based on number of inputs.
-func newFrame(numframes int) *frame {
-	return &frame{expected: numframes}
-}
-
-// send all complete frames. Returns next incomplete frame.
-func send(f *frame, out chan *frame, cancel chan struct{}) *frame {
-	for f != nil && f.isComplete() {
-		select {
-		case out <- f:
-			f = f.next
-		case <-cancel:
-			return nil
-		}
+func newFrame(numInputs int32, numChannels int) *frame {
+	return &frame{
+		expected: int(numInputs),
+		buffer:   make([][]float64, numChannels),
 	}
-	return f
 }
 
 // resetExpectations for all frames after this one.
-func resetExpectations(f *frame, e int) {
+func resetExpectations(out chan *frame, f *frame, e int32) {
 	for f != nil {
-		f.expected = e
+		f.Lock()
+		f.expected = int(e)
+		f.Unlock()
+		if f.expected == f.summed && f.expected > 0 {
+			out <- f
+		}
 		f = f.next
 	}
 }
